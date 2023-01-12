072741866ba7c8b3edddfebdf9ca0463cb659588
Jeffrey Ramo <matheusunitt@gmail.com>
2023-01-11 22:19:45 -0300

    Adiciona rotas e import da biblioteca Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./styles/global.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
